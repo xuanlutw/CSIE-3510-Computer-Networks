9aefda695aac94a1114bc21bf1f792e0539b3ffa
xuanlutw <b05202043@csie.ntu.edu.tw>
2018-12-29 13:35:33 +0800

    add server log mutex

--- project2/include/init.h
+++ project2/include/init.h
@@ -1,11 +1,13 @@
 #ifndef INIT_H
 #define INIT_H
 
+#include <pthread.h>
 #include "user.h"
 
 typedef struct {
     User_info* user_info;
     int port;
+    pthread_mutex_t log_lock;
 } Share_data;
 
 Share_data* init_share_data(int port);
--- project2/include/utils.h
+++ project2/include/utils.h
@@ -6,8 +6,14 @@
 #define BUF_SIZE 1024
 
 // General
-#define server_log(...) do{dump_time();printf(__VA_ARGS__);printf("\n");}while(0)
-void dump_time();
+#define server_log(LOG_LOCK, ...)   do{\
+                                    pthread_mutex_lock(LOG_LOCK);\
+                                    dump_time();\
+                                    printf(__VA_ARGS__);\
+                                    printf("\n");\
+                                    pthread_mutex_unlock(LOG_LOCK);\
+                                }while(0)
+void dump_time(pthread_mutex_t *log_lock);
 
 // Net
 ssize_t crypto_recv(int key, int socket, void *buffer, size_t length, int flags);
